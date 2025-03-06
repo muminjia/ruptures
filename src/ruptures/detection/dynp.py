a9b0be4d1714abaa297d1f8642752e965a7e74c2
muminjia <mjia@bu.edu>
2025-03-05 21:03:05 -0500

    More changes

--- src/ruptures/detection/dynp.py
+++ src/ruptures/detection/dynp.py
@@ -129,7 +129,7 @@ class Dynp(BaseEstimator):
         """
         # raise an exception in case of impossible segmentation configuration
         if not sanity_check(
-            n_samples=self.cost.signal.shape[0],
+            n_samples=self.cost.signal1.shape[0],
             n_bkps=n_bkps,
             jump=self.jump,
             min_size=self.min_size,
